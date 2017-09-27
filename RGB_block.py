e517f0cd22fef7647efc052e5e1bbd022543e640
Scott Clay <sclay@n.io>
2017-09-27 14:34:01 -0600

    Fix test, Remove unused import

---
+++ RGB_block.py
@@ -0,0 +1,30 @@
+from nio.block.base import Block
+from nio.properties import VersionProperty
+from nio.signal.base import Signal
+import time
+import Adafruit_TCS34725
+
+
+class RGB(Block):
+
+    version = VersionProperty('0.1.0')
+
+    def __init__(self):
+        super().__init__()
+        self.tcs = None
+
+    def configure(self,context):
+        super().configure(context)
+        self.tcs = Adafruit_TCS34725.TCS34725()
+        self.tcs.set_interrupt(False)
+
+    def process_signals(self, signals):
+        for signal in signals:
+            r, g, b, c = self.tcs.get_raw_data()
+            color_temp = Adafruit_TCS34725.calculate_color_temperature(r, g, b)
+            lux = Adafruit_TCS34725.calculate_lux(r, g, b)
+
+        self.notify_signals([ Signal( {"red": r,
+                                       "green" : g,
+                                       "blue" : b,
+                                       "clear" : c })])
--- tests/test_RGB_block.py
+++ tests/test_RGB_block.py
@@ -1,17 +1,14 @@
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..TCS34725_block import RGB
+from ..RGB_block import RGB
 from unittest.mock import patch, MagicMock, Mock
-
 
 
 class TestTCS34725(NIOBlockTestCase):
 
     def test_process_signals(self):
         """Signals pass through block unmodified."""
-        #blk = TCS34725()
-        #mockguy.return_value.get_raw_data.return_value = MagicMock()
         with patch('Adafruit_TCS34725.TCS34725') as mockguy:
             mockdata = [1, 2, 3, 4]
             mock_get_raw_data = mockguy.return_value.get_raw_data
@@ -19,10 +16,13 @@ class TestTCS34725(NIOBlockTestCase):
             blk = RGB()
             self.configure_block(blk, {})
             blk.start()
-            blk.process_signals([Signal({"hello": "n.io"})])
+            blk.process_signals([Signal()])
             blk.stop()
             self.assertTrue(mock_get_raw_data.call_count)
             self.assert_num_signals_notified(1)
             self.assertDictEqual(
                 self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-                {"hello": "n.io"})
+                {'red': 1,
+                 'green': 2,
+                 'blue': 3,
+                 'clear': 4})
